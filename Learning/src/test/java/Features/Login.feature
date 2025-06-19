708a1503a6d5067f8899f372b017cae5fbd91bce
RuchitaDas <RuchitaDas@OR-LP-075>
2025-06-19 08:49:23 +0530

    Affiliate and Admin Automation script

---
+++ Learning/src/test/java/Features/Login.feature
@@ -0,0 +1,41 @@
+Feature: Login as Affiliate
+
+#simple data driven testing
+Scenario: Positive Login
+
+Given User login page
+When title is JumpSpark
+Then user enters "ruchita.das@omnireach.app" and "JumpSpark#24"
+Then user click on login button
+Then login as affiliate
+Then close the browser
+
+#scenario outline testing
+Scenario Outline: Empty Scenario
+
+Given User login page
+When title is JumpSpark
+Then user enters "<username>" and "<password>"
+Then user click on login button
+Then login as affiliate
+Then close the browser
+ 
+#keyword to give all the values in form of table
+#example keyword is help to use the same data everypossible 
+Examples: 
+		| username | password|
+		|          | JumpSpark#24|
+		|ruchita.das@omnireach.app|        |
+		|ruchita.das@omnireach.app | JumpSpark#24|		
+		
+#using test data
+#Scenario: Negative Login
+#
+#Given User login page
+#When title is JumpSpark
+#Then user enters username and password
+#|ruchitadas1223| rbeigeruernk|
+#|ewrrrrefretgtgthtyhyjfujysrfgefre@gmail.com| fdkrgtuitrgn|
+#Then user click on login button
+#Then login as affiliate
+#Then close the browser
---
+++ Learning/src/test/java/StepDefination/LoginAffiliate.java
@@ -0,0 +1,74 @@
+package StepDefination;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginAffiliate {
+	WebDriver driver;
+	@Given("^User login page$")
+	public void user_login_page() {
+		WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://preprodjs.wpenginepowered.com/dashboard/");
+	}
+	
+	@When("^title is JumpSpark$")
+	public void title_is_JumpSpark() {
+		String title = driver.getTitle();
+		System.out.println(title);
+		Assert.assertEquals("login – Jump Spark", title);
+	}
+//simple and outline data driven testing	
+	@Then("^user enters \"(.*)\" and \"(.*)\"$") 
+	public void user_enters_email_id_and_password(String username, String password) {		driver.findElement(By.id("email")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
+	}
+	
+//	@Then("^user enters username and password$")
+//	public void user_enter_username_and_password(DataTable credentials) {
+//	    List<List<String>> data = credentials.asLists(String.class);
+//	    driver.findElement(By.id("email")).sendKeys(data.get(0).get(0));
+//	    driver.findElement(By.id("password")).sendKeys(data.get(0).get(1));
+//	}
+
+	
+	@Then ("^user click on login button$")
+	public void user_click_on_login_button() {
+		WebElement loginBtn = driver.findElement(By.xpath("//button[normalize-space()='Log In']"));
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("arguments[0].click();", loginBtn);
+	}
+	@Then("^login as affiliate$")
+	public void login_affiliate() {
+		
+		// Why use wait -> so that it can take its time load the page 
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.titleIs("dashboard – Jump Spark"));
+		
+		//storing the title in a variable
+		String a= driver.getTitle();
+		System.out.println(a);
+		Assert.assertEquals("dashboard – Jump Spark", a);
+	}
+	@Then("^close the browser$")
+	public void close_the_browser() {
+		driver.quit();
+	}
+
+	
+}
